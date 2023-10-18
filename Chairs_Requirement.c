26daf0f8f4b0984d29e55d60a39dc65ddc353767
preetham054 <147487397+preetham054@users.noreply.github.com>
2023-10-18 06:42:58 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     if(a>b)     {         c=a-b;         printf("%d",c);     }     else         printf("%d",0);     }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        c=a-b;
+        printf("%d",c);
+    }
+    else
+        printf("%d",0);
+    }
